0a1a78f6fa57604d9d943afabfd656cfcfc1d3cf
Santiago Guex <wtomk44@gmail.com>
2022-10-15 12:17:18 -0300

    hello python, types string, int, boolean, complex and float

---
+++ hello.py
@@ -0,0 +1,19 @@
+#esto es un comentario
+# Nuestro hola mundo en Python
+print("Hello World!")
+print('Hello Python!') #Cadena de texto (puede ser con comillas dobles o comillas simples)
+
+"""
+Este es un 
+comentario
+en varias lineas!
+se puede hacer con tres lineas simples '''
+"""
+
+#Consultar el tipo de dato:
+#print(type(value))
+print(type("Soy un dato str")) #Cadena de texto (puede ser con comillas dobles o comillas simples)
+print(type(5)) #Tipo int, numeros simples
+print(type(True)) #Tipo boolean, True o False
+print(type(6.12)) #Tipo float, numeros decimales
+print(type(1 + 2j)) # Tipo complex
